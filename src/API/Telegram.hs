def90a10a92d796e421489be1cd71bd7ccd3b44b
SpaceOyster <spaceoyster@disroot.org>
2021-03-29 18:51:22 +0500

    added Telegram API client module with some simple functions

---
+++ src/API/Telegram.hs
@@ -0,0 +1,36 @@
+module API.Telegram where
+
+import Control.Exception (handle)
+import Control.Monad ((=<<))
+import Control.Monad.Catch (MonadThrow(..))
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Network.HTTP.Client
+    ( HttpException(..)
+    , Manager(..)
+    , Request(..)
+    , brRead
+    , httpLbs
+    , newManager
+    , parseRequest
+    , responseBody
+    , responseStatus
+    , withResponse
+    )
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import System.Environment (getEnv)
+
+makeRequest :: MonadThrow m => String -> String -> m Request
+makeRequest key method =
+    parseRequest $ "https://api.telegram.org/bot" ++ key ++ "/" ++ method
+
+getUpdates :: Manager -> IO L8.ByteString
+getUpdates manager = do
+    apiKey <- getEnv "TG_API"
+    req <- makeRequest apiKey "getUpdates"
+    res <- httpLbs req manager
+    return $ responseBody res
+
+testMakeRequest = do
+    man <- newManager tlsManagerSettings
+    getUpdates man
