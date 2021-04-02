ae9d0d2f5dc79945e40623cdea0d1c35c229dd9d
SpaceOyster <spaceoyster@disroot.org>
2021-04-02 16:14:44 +0500

    removed API.Telegram.Types.Status type (not needed)

--- src/API/Telegram/Types.hs
+++ src/API/Telegram/Types.hs
@@ -7,14 +7,6 @@ module API.Telegram.Types where
 import Data.Aeson (FromJSON(..), ToJSON(..), (.:), decode, encode, withObject)
 import GHC.Generics
 
-data Status =
-    Status
-        { ok :: Bool
-        , error_code :: Int
-        , description :: String
-        }
-    deriving (Show, Generic, ToJSON, FromJSON)
-
 data Response
     = Error
           { error_code :: Int
