c0e6c2375a2bad52d6652199edd9093a2baf14cd
SpaceOyster <spaceoyster@disroot.org>
2021-04-02 14:12:44 +0500

    added API.Telegram.Types.Respose and FromJSON instance

--- src/API/Telegram/Types.hs
+++ src/API/Telegram/Types.hs
@@ -14,3 +14,21 @@ data Status =
         , description :: String
         }
     deriving (Show, Generic, ToJSON, FromJSON)
+
+data Response
+    = Error
+          { error_code :: Int
+          , description :: String
+          }
+    | Result
+          { result :: [String]
+          }
+    deriving (Show)
+
+instance FromJSON Response where
+    parseJSON =
+        withObject "Response" $ \o -> do
+            ok <- o .: "ok"
+            if ok
+                then Result <$> o .: "result"
+                else Error <$> o .: "error_code" <*> o .: "description"
