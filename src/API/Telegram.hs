1a6ee200bf6c2a3f13920e492661b790b3063ec0
SpaceOyster <spaceoyster@disroot.org>
2021-04-22 23:28:38 +0500

    repeatKeyboard changed to be the way it ment to

--- src/API/Telegram.hs
+++ src/API/Telegram.hs
@@ -142,11 +142,7 @@ sendMessage handle chatId msg = do
 
 repeatKeyboard :: InlineKeyboardMarkup
 repeatKeyboard =
-    InlineKeyboardMarkup
-        [ [button 1, button 2, button 3]
-        , [button 4, button 5, button 6]
-        , [button 7, button 8, button 9]
-        ]
+    InlineKeyboardMarkup [[button 1, button 2, button 3, button 4, button 5]]
   where
     button x = InlineKeyboardButton {text = show x, callback_data = show x}
 
