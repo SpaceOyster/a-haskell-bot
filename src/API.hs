5cdcf81e228c240c679a0bd57deb2b692a4f4f27
SpaceOyster <spaceoyster@disroot.org>
2021-04-27 16:12:12 +0500

    added abstract API module

---
+++ src/API.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DuplicateRecordFields, NamedFieldPuns #-}
+
+module API where
+
+import Control.Monad.Catch (MonadThrow(..))
+import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Function ((&))
+import qualified HTTP
+
+data Config =
+    Config
+        { key :: String
+        , helpMessage :: String
+        , greeting :: String
+        , repeatPrompt :: String
+        }
+
+data Handle m =
+    Handle
+        { http :: HTTP.Handle m
+        , helpMessage :: String
+        , greeting :: String
+        , repeatPrompt :: String
+        }
+
+get :: (MonadThrow m) => Handle m -> String -> m L8.ByteString
+get hAPI = hAPI & http & HTTP.get
+
+post :: (MonadThrow m) => Handle m -> String -> L8.ByteString -> m L8.ByteString
+post hAPI = hAPI & http & HTTP.post
