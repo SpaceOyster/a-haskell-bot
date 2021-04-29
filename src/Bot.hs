d945974bbebb6377e09e5bd41ca4d2e44896d927
SpaceOyster <spaceoyster@disroot.org>
2021-04-29 20:25:26 +0500

    Bot module changed to use abstract getUpdates

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,15 +1,20 @@
 module Bot where
 
+import qualified API
 import API.Telegram
 import Control.Concurrent (threadDelay)
 import qualified Data.ByteString.Lazy.Char8 as L8
+import Data.Function ((&))
 import HTTP
+import Utils
 
-doBotThing :: Handle IO -> IO [L8.ByteString]
-doBotThing handle = getUpdates handle >>= reactToUpdates handle
+doBotThing :: API.Handle IO -> IO [L8.ByteString]
+doBotThing hAPI = do
+    json <- hAPI & API.sendRequest $ hAPI & API.getUpdates
+    hAPI `reactToUpdates` json
 
-loop :: Handle IO -> IO ()
-loop handle = do
-    doBotThing handle
+loop :: API.Handle IO -> IO ()
+loop hAPI = do
+    doBotThing hAPI
     threadDelay 5000000
-    loop handle
+    loop hAPI
