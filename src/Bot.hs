b669791b45f0113e8ae405a784282c4bced029f5
SpaceOyster <spaceoyster@disroot.org>
2021-05-18 19:53:53 +0500

    commented out bot algorithm in Bot module

--- src/Bot.hs
+++ src/Bot.hs
@@ -18,19 +18,20 @@ data Handle m state =
         , defaultRepeat :: Int
         }
 
-doBotThing :: API.Handle IO TG.HState -> IO [L8.ByteString]
-doBotThing hAPI = do
-    req <- hAPI & TG.getUpdates
-    json <- hAPI & API.sendRequest $ req
-    requests <- TG.reactToUpdates hAPI json
-    mapM (API.sendRequest hAPI) requests
-
-loop :: API.Handle IO TG.HState -> IO ()
-loop hAPI = do
-    doBotThing hAPI
-    threadDelay 5000000
-    loop hAPI
-
+{-
+ - doBotThing :: API.Handle IO -> IO [L8.ByteString]
+ - doBotThing hAPI = do
+ -     req <- hAPI & TG.getUpdates
+ -     json <- hAPI & API.sendRequest $ req
+ -     requests <- TG.reactToUpdates hAPI json
+ -     mapM (API.sendRequest hAPI) requests
+ - 
+ - loop :: API.Handle IO -> IO ()
+ - loop hAPI = do
+ -     doBotThing hAPI
+ -     threadDelay 5000000
+ -     loop hAPI
+ -}
 hGetState :: Handle m state -> IO state
 hGetState = readIORef . state
 
