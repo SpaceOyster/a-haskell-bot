2d55628a898e6dff2cf32270d9a705a4cfc64a32
SpaceOyster <spaceoyster@disroot.org>
2021-05-18 16:19:31 +0500

    added reactToUpdate to Bot.Telegram

--- src/Bot/Telegram.hs
+++ src/Bot/Telegram.hs
@@ -8,6 +8,8 @@ import API.Telegram.Types
 import Bot
 import Data.IORef (IORef)
 import qualified Data.Map as Map (Map, alter, findWithDefault)
+import qualified Exceptions as Priority (Priority(..))
+import Exceptions (BotException(..))
 
 data BotState =
     BotState
@@ -27,3 +29,14 @@ setUserSettings hBot user repeats = do
     hBot `Bot.hSetState` \st ->
         let usettings = Map.alter (const $ Just repeats) user $ userSettings st
          in st {userSettings = usettings}
+
+reactToUpdate :: Handle IO BotState -> Update -> IO [API.Request]
+reactToUpdate hBot update = do
+    let qu = TG.qualifyUpdate update
+    case qu of
+        TG.ECommand msg -> (: []) <$> reactToCommand hBot msg
+        TG.EMessage msg -> reactToMessage hBot msg
+        TG.ECallback cq -> (: []) <$> reactToCallback hBot cq
+        TG.EOther Update {update_id} ->
+            throwM $
+            Ex Priority.Info $ "Unknown Update Type. Update: " ++ show update_id
