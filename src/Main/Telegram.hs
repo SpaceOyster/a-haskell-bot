c41de97717baee7931971c83c07e52a1fdd8a0b0
SpaceOyster <spaceoyster@disroot.org>
2021-06-10 23:24:17 +0500

    added FromJSON instance for Main.Telegram.AppConfig

--- src/Main/Telegram.hs
+++ src/Main/Telegram.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main.Telegram where
 
 import qualified Bot
 import qualified Bot.Telegram as TG
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
+import qualified Data.Aeson as A
+import qualified Data.ByteString.Lazy as BL
 
 loop :: Bot.Handle IO -> IO ()
 loop hBot =
@@ -16,3 +21,25 @@ data AppConfig =
         { poll_period :: Integer
         , telegram :: TG.Config
         }
+
+instance A.FromJSON AppConfig where
+    parseJSON =
+        A.withObject "FromJSON Main.AppConfig" $ \o -> do
+            defaults <- o A..: "defaults"
+            repeats <- defaults A..: "repeats"
+            poll_period <- defaults A..: "poll-period"
+            telegramO <- o A..: "telegram"
+            tg_api_key <- telegramO A..: "api-key"
+            strings <- o A..: "strings"
+            help <- strings A..: "help"
+            greeting <- strings A..: "greeting"
+            repeat <- strings A..: "repeat"
+            let telegram =
+                    TG.Config
+                        { TG.key = tg_api_key
+                        , TG.helpMessage = help
+                        , TG.greeting = greeting
+                        , TG.repeatPrompt = repeat
+                        , TG.defaultRepeat = repeats
+                        }
+            return $ AppConfig {..}
