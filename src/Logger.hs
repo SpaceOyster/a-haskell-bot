17f8ee6a84d685d86a6df4976038447290adcaa6
SpaceOyster <spaceoyster@disroot.org>
2021-06-25 00:06:52 +0500

    added helper loggers with specified Verbosity

--- src/Logger.hs
+++ src/Logger.hs
@@ -13,7 +13,7 @@ import qualified Data.Text as T (Text, pack)
 import qualified Data.Text.IO as T (hPutStrLn)
 import qualified Data.Time.Format as Time (defaultTimeLocale, formatTime)
 import qualified Data.Time.LocalTime as Time (getZonedTime)
-import Prelude hiding (log)
+import Prelude hiding (error, log)
 import qualified System.IO as IO
 
 data Verbosity
@@ -65,6 +65,9 @@ log Handle {..} v s =
                  ts <> " " <> verbToText v <> " " <> s
             else noLog
 
+log' :: MonadIO m => Handle -> Verbosity -> String -> m ()
+log' hLog v = log hLog v . T.pack
+
 noLog :: (Monad m) => m ()
 noLog = pure ()
 
@@ -98,3 +101,27 @@ timeStamp :: IO T.Text
 timeStamp = do
     time <- Time.getZonedTime
     pure . T.pack $ Time.formatTime Time.defaultTimeLocale "%b %d %X %Z" time
+
+debug :: MonadIO m => Handle -> T.Text -> m ()
+debug h = log h Debug
+
+info :: MonadIO m => Handle -> T.Text -> m ()
+info h = log h Info
+
+warning :: MonadIO m => Handle -> T.Text -> m ()
+warning h = log h Warning
+
+error :: MonadIO m => Handle -> T.Text -> m ()
+error h = log h Error
+
+debug' :: MonadIO m => Handle -> String -> m ()
+debug' h = log h Debug . T.pack
+
+info' :: MonadIO m => Handle -> String -> m ()
+info' h = log h Info . T.pack
+
+warning' :: MonadIO m => Handle -> String -> m ()
+warning' h = log h Warning . T.pack
+
+error' :: MonadIO m => Handle -> String -> m ()
+error' h = log h Error . T.pack
