46f166c5a8ca1216dcaba3fc42f0efbcd55f3334
SpaceOyster <spaceoyster@disroot.org>
2021-07-08 18:27:25 +0500

    added API.Vkontakte module with some basic stuff

---
+++ src/API/Vkontakte.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+module API.Vkontakte
+    (
+    ) where
+
+import API
+import Data.Aeson
+import Data.Function ((&))
+import Data.IORef (modifyIORef', newIORef, readIORef)
+import GHC.Generics
+import qualified HTTP
+
+data Config =
+    Config
+        { key :: String
+        , group_id :: Integer
+        , v :: String
+        }
+
+new :: Config -> IO Handle
+new cfg@Config {..} = do
+    http <- HTTP.new $ HTTP.Config {}
+    getLongPollServer http cfg
+    lastUpdate <- newIORef 0
+    pure $ Handle {http, hLog = undefined, lastUpdate, baseURL = ""}
+
+data PollServer =
+    PollServer
+        { key :: String
+        , server :: String
+        , ts :: Integer
+        }
+    deriving (Show, Generic, FromJSON)
+
+data APIResponse =
+    Response
+        { response :: PollServer
+        }
+    deriving (Show, Generic, FromJSON)
+
+getLongPollServer :: HTTP.Handle -> Config -> IO (Maybe APIResponse)
+getLongPollServer http Config {..} = do
+    json <-
+        http & HTTP.get $
+        "https://api.vk.com/method/groups.getLongPollServer?v=" <>
+        v <> "&access_token=" <> key <> "&group_id=" <> show group_id
+    pure $ decode json
