8117e0fa0b75d16484ad00b953f3b359e15754d2
SpaceOyster <spaceoyster@disroot.org>
2021-07-13 23:57:06 +0500

    added URLParsing BotException

--- src/Exceptions.hs
+++ src/Exceptions.hs
@@ -15,11 +15,13 @@ data Priority
     | Error
     deriving (Eq, Enum, Show)
 
-data BotException =
-    Ex Priority !String
+data BotException
+    = Ex Priority !String
+    | URLParsing !String
     deriving (Typeable)
 
 instance Exception BotException
 
 instance Show BotException where
     show (Ex priority description) = "[" ++ show priority ++ "] " ++ description
+    show (URLParsing description) = "URLParsing: " <> description
