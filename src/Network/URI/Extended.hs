e3d30c0971763ab34b28082ae895a17e2b38e002
SpaceOyster <spaceoyster@disroot.org>
2021-07-14 00:05:34 +0500

    added addPath function to Network.URI.Extended

--- src/Network/URI/Extended.hs
+++ src/Network/URI/Extended.hs
@@ -1,6 +1,10 @@
 module Network.URI.Extended
     ( module Network.URI
+    , addPath
     ) where
 
 import Network.URI
 
+
+addPath :: URI -> String -> URI
+addPath uri p = uri {uriPath = uriPath uri <> p}
