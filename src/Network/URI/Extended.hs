553d55ac64c6b7dde47dea743e8c3280a8c8ebd7
SpaceOyster <spaceoyster@disroot.org>
2021-07-14 00:08:01 +0500

    added function to addQueryParams to URI

--- src/Network/URI/Extended.hs
+++ src/Network/URI/Extended.hs
@@ -1,11 +1,23 @@
 module Network.URI.Extended
     ( module Network.URI
     , addPath
+    , addQueryParams
     ) where
 
+import Data.List (intercalate)
 import Network.URI
 
 type QueryParams = [(String, Maybe String)]
 
 addPath :: URI -> String -> URI
 addPath uri p = uri {uriPath = uriPath uri <> p}
+
+addQueryParams :: URI -> QueryParams -> URI
+addQueryParams uri [] = uri
+addQueryParams uri qs =
+    case uriQuery uri of
+        [] -> uri {uriQuery = '?' : stringifyQuery qs}
+        q -> uri {uriQuery = q <> ('&' : stringifyQuery qs)}
+
+stringifyQuery :: QueryParams -> String
+stringifyQuery = intercalate "&" . fmap (\(k, v) -> k <> maybe "" ('=' :) v)
