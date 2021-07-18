b87bea656699ce0470b75f490a878a5cee408cd2
SpaceOyster <spaceoyster@disroot.org>
2021-07-18 15:28:56 +0500

    added API.Vkontakte.baseURI function

--- src/API/Vkontakte.hs
+++ src/API/Vkontakte.hs
@@ -9,11 +9,14 @@ module API.Vkontakte
     ) where
 
 import API
+import Control.Monad.Catch (MonadThrow(..))
 import Data.Aeson
 import Data.Function ((&))
 import Data.IORef (modifyIORef', newIORef, readIORef)
+import qualified Exceptions as Ex
 import GHC.Generics
 import qualified HTTP
+import qualified Network.URI.Extended as URI
 import Utils (throwDecode)
 
 data Config =
@@ -43,6 +46,15 @@ data APIResponse =
         { response :: PollServer
         }
     deriving (Show, Generic, FromJSON)
+
+makeBaseURI :: HTTP.Handle -> Config -> IO URI.URI
+makeBaseURI http cfg@Config {..} = do
+    res <- getLongPollServer http cfg
+    let PollServer {..} = response res
+    maybe ex pure . URI.parseURI $
+        server <> "/?act=a_check&key=" <> key <> "&ts=" <> show ts <> "&wait=25"
+  where
+    ex = throwM $ Ex.URLParsing "Unable to parse Vkontakte Long Poll URL"
 
 getLongPollServer :: HTTP.Handle -> Config -> IO APIResponse
 getLongPollServer http Config {..} = do
