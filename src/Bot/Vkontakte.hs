84c85665de498dcc26d7e69792282742072111b2
SpaceOyster <spaceoyster@disroot.org>
2021-07-19 23:37:53 +0500

    added Bot.Vkontakte module

---
+++ src/Bot/Vkontakte.hs
@@ -0,0 +1,26 @@
+module Bot.Vkontakte where
+
+import qualified API
+import qualified API.Vkontakte as VK
+import Bot hiding (strings)
+import qualified Bot (strings)
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+data Config =
+    Config
+        { key :: String
+        , echoMultiplier :: Int
+        , strings :: Bot.Strings
+        }
+    deriving (Show)
+
+new :: Config -> IO Handle
+new cfg = undefined
+
+withHandle :: Config -> (Handle -> IO a) -> IO a
+withHandle config io = do
+    hBot <- new config
+    io hBot
+
+doBotThing :: Handle -> IO (L8.ByteString)
+doBotThing hBot = undefined
