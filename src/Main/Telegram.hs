222573aa67cb27c3fb6e9653fd1eb08c51ecb7c2
SpaceOyster <spaceoyster@disroot.org>
2021-10-05 16:37:08 +0500

    made Main.Telegram more abstract with HandleConfig

--- src/Main/Telegram.hs
+++ src/Main/Telegram.hs
@@ -3,9 +3,7 @@
 
 module Main.Telegram where
 
-import qualified API.Telegram as TG (APIState)
 import qualified Bot
-import qualified Bot.Telegram as TG (doBotThing, withHandle)
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Data.AppConfig
@@ -13,10 +11,10 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Logger
 import qualified Utils as U (throwDecode)
 
-loop :: Bot.Handle TG.APIState -> Int -> IO ()
+loop :: (Bot.BotHandle a) => a -> Int -> IO ()
 loop hBot period =
     forever $ do
-        TG.doBotThing hBot
+        Bot.doBotThing hBot
         threadDelay period
 
 run :: FilePath -> IO ()
@@ -28,4 +26,4 @@ run configPath = do
         Logger.info' hLog $
             "API Polling period is " <>
             show (fromIntegral poll_period / 1000) <> "ms"
-        TG.withHandle telegram hLog $ flip loop poll_period
+        Bot.withHandle telegram hLog $ flip loop poll_period
