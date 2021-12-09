16fc02d05a03998331f73fc333cbc495b730a49f
SpaceOyster <spaceoyster@disroot.org>
2021-12-09 19:09:38 +0500

    simplified app.Main.hs (it's not really needed)

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,4 @@
 module Main where
 
-import qualified Bot.Telegram as TG (withHandle)
-import qualified Main.Telegram as TG
-
 main :: IO ()
-main = do
-  TG.withHandle TG.loop
+main = putStrLn "main function executed"
