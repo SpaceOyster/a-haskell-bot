ab902151aff59b40c20afafd298ebbbaffdec8f3
SpaceOyster <spaceoyster@disroot.org>
2021-12-23 18:55:58 +0500

    defined App.Monad.App

--- src/App/Monad.hs
+++ src/App/Monad.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module App.Monad where
 
+import Control.Monad.Reader
 import Data.Kind (Type)
 import qualified Logger
 
@@ -9,3 +11,11 @@ newtype Env (m :: Type -> Type) =
     Env
         { envLogger :: Logger.Handle
         }
+
+type AppEnv = Env App
+
+newtype App a =
+    App
+        { unApp :: ReaderT AppEnv IO a
+        }
+    deriving (Functor, Applicative, Monad, MonadIO, MonadReader AppEnv)
