2f188afc4c3b6d9c86a7181c2091788f749015e4
SpaceOyster <spaceoyster@disroot.org>
2021-12-23 23:39:10 +0500

    lifted Bot.Vkontakte functions to MonadIO

--- src/Bot/Vkontakte.hs
+++ src/Bot/Vkontakte.hs
@@ -14,6 +14,7 @@ import qualified API.Vkontakte as VK
 import qualified Bot
 import Control.Monad (replicateM)
 import Control.Monad.Catch (MonadThrow(..))
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Types as A (parseMaybe)
 import Data.Function ((&))
@@ -49,10 +50,10 @@ instance IsHandle (Bot.Handle VK.Handle) Config where
 
 instance Bot.BotHandle (Bot.Handle VK.Handle) where
     type Update (Bot.Handle VK.Handle) = VK.GroupEvent
-    fetchUpdates :: Bot.Handle VK.Handle -> IO [VK.GroupEvent]
+    fetchUpdates :: MonadIO m => Bot.Handle VK.Handle -> m [VK.GroupEvent]
     fetchUpdates hBot@Bot.Handle {hAPI} = do
         L.logInfo hBot "Vkontakte: fetching Updates"
-        VK.runMethod hAPI VK.GetUpdates >>= VK.extractUpdates
+        liftIO $ VK.runMethod hAPI VK.GetUpdates >>= VK.extractUpdates
     data Entity (Bot.Handle VK.Handle) = EMessage VK.Message
                                    | ECommand VK.Message
                                    | ECallback VK.CallbackEvent
@@ -64,22 +65,27 @@ instance Bot.BotHandle (Bot.Handle VK.Handle) where
         | otherwise = EMessage m
     qualifyUpdate (VK.MessageEvent c) = ECallback c
     qualifyUpdate _ = EOther VK.Other -- TODO
-    reactToUpdate :: Bot.Handle VK.Handle -> VK.GroupEvent -> IO [VK.Response]
+    reactToUpdate ::
+           MonadIO m => Bot.Handle VK.Handle -> VK.GroupEvent -> m [VK.Response]
     reactToUpdate hBot@Bot.Handle {hLog} update = do
         L.logInfo hLog $ "VK got Update: " <> T.tshow update
         let qu = Bot.qualifyUpdate update
-        case qu of
-            ECommand msg -> (: []) <$> reactToCommand hBot msg
-            EMessage msg -> reactToMessage hBot msg
-            ECallback cq -> reactToCallback hBot cq
-            EOther _ -> throwM $ Ex Priority.Info "Unknown Update Type."
+        liftIO $
+            case qu of
+                ECommand msg -> (: []) <$> reactToCommand hBot msg
+                EMessage msg -> reactToMessage hBot msg
+                ECallback cq -> reactToCallback hBot cq
+                EOther _ -> throwM $ Ex Priority.Info "Unknown Update Type."
     type Message (Bot.Handle VK.Handle) = VK.Message
     execCommand ::
-           Bot.Handle VK.Handle -> Bot.Command -> (VK.Message -> IO VK.Response)
+           MonadIO m
+        => Bot.Handle VK.Handle
+        -> Bot.Command
+        -> (VK.Message -> m VK.Response)
     execCommand hBot@Bot.Handle {..} cmd VK.Message {..} = do
         let address = peer_id
         prompt <- Bot.repeatPrompt hBot $ Just $ VK.User from_id
-        VK.runMethod hAPI $
+        liftIO . VK.runMethod hAPI $
             case cmd of
                 Bot.Start -> VK.SendTextMessage address (Bot.greeting strings)
                 Bot.Help -> VK.SendTextMessage address (Bot.help strings)
