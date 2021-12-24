9d5e558d2e47a21996b68c7d2eccae16e0c3ccb2
SpaceOyster <spaceoyster@disroot.org>
2021-12-24 23:13:35 +0500

    updated Main.Vkontakte to use App.Monad

--- src/Main/Vkontakte.hs
+++ src/Main/Vkontakte.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main.Vkontakte where
 
 import App.Config
+import qualified App.Monad as App
 import qualified Bot
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader
 import qualified Data.Aeson.Extended as A (throwDecode)
 import qualified Data.ByteString.Lazy as BL
 import Data.List (intercalate)
@@ -17,23 +20,26 @@ import qualified Logger as L
 import qualified System.Environment as E
 import qualified System.Exit as Exit (die)
 
-loop :: (MonadIO m, Bot.BotHandle a) => a -> Int -> m ()
-loop hBot period = liftIO . forever $ Bot.doBotThing hBot >> threadDelay period
+loop ::
+       (MonadIO m, Bot.BotHandle a, MonadReader env m, App.Has L.Handle env)
+    => a
+    -> Int
+    -> m ()
+loop hBot period = forever $ Bot.doBotThing hBot >> liftIO (threadDelay period)
 
-run :: AppConfig -> IO ()
-run AppConfig {..} = do
-    L.withHandle logger $ \hLog -> do
-        L.logInfo hLog "Initiating Main Bot loop"
-        L.logInfo hLog $
-            "API Polling period is " <>
-            T.tshow (fromIntegral poll_period / 1000 :: Double) <> "ms"
-        withHandle vkontakte hLog $ flip loop poll_period
-
+-- run :: AppConfig -> IO ()
+-- run AppConfig {..} = do
+--     L.withHandle logger $ \hLog -> do
+--         L.logInfo hLog "Initiating Main Bot loop"
+--         L.logInfo hLog $
+--             "API Polling period is " <>
+--             T.tshow (fromIntegral poll_period / 1000 :: Double) <> "ms"
+--         withHandle vkontakte hLog $ flip loop poll_period
 main :: IO ()
 main = do
     args <- E.getArgs
     case args of
-        cfgPath:_as -> readConfig cfgPath >>= run
+        cfgPath:_as -> readConfig cfgPath >>= runWithApp
         _ -> Exit.die usagePrompt
 
 readConfig :: FilePath -> IO AppConfig
@@ -52,3 +58,14 @@ usagePrompt =
         , "FILE - is a config file formatted as json, see example config here:"
         , "INSTRUCTIONS TO FIND EXAMPLE CONFIG"
         ]
+
+runWithApp :: AppConfig -> IO ()
+runWithApp AppConfig {..} = do
+    L.withHandle logger $ \hLog -> do
+        L.logInfo hLog "Initiating Main Bot loop"
+        L.logInfo hLog $
+            "API Polling period is " <>
+            T.tshow (fromIntegral poll_period / 1000 :: Double) <> "ms"
+        let env = App.Env {envLogger = hLog}
+        hBot <- new vkontakte hLog
+        App.unApp (loop hBot poll_period) `runReaderT` env
