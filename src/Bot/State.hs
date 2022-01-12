bb54614c2bda827edda2ef061edbe7250c8c7b1d
SpaceOyster <spaceoyster@disroot.org>
2022-01-12 15:22:56 +0500

    simplified Bot.State to just use MonadIO

--- src/Bot/State.hs
+++ src/Bot/State.hs
@@ -13,17 +13,10 @@ module Bot.State
     , setUserMultiplier
     ) where
 
-import App.Monad (envLogDebug, envLogInfo)
-import Control.Monad.Catch (MonadThrow)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Reader (MonadReader)
-import Data.Function ((&))
-import Data.Has (Has(..))
 import qualified Data.Hashable as H
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
 import qualified Data.Map as Map (Map, alter, findWithDefault)
-import qualified Data.Text.Extended as T
-import qualified Logger
 
 newtype BotState =
     BotState
@@ -46,34 +39,13 @@ new Config {echoMultiplier} = do
     state <- liftIO $ newIORef BotState {userSettings = mempty}
     pure Handle {state, echoMultiplier}
 
-hGetState ::
-       (MonadIO m, MonadThrow m, MonadReader env m, Has Logger.Handle env)
-    => Handle
-    -> m BotState
-hGetState hState = do
-    envLogDebug "Getting BotState"
-    liftIO . readIORef $ state hState
+hGetState :: (MonadIO m) => Handle -> m BotState
+hGetState hState = liftIO . readIORef $ state hState
 
-hSetState ::
-       (MonadIO m, MonadThrow m, MonadReader env m, Has Logger.Handle env)
-    => Handle
-    -> (BotState -> BotState)
-    -> m ()
-hSetState hState f = do
-    envLogDebug "Setting BotState"
-    envLogDebug "Appying state BotState mutating function"
-    liftIO $ state hState `modifyIORef` f
+hModifyState :: (MonadIO m) => Handle -> (BotState -> BotState) -> m ()
+hModifyState hState f = liftIO $ state hState `modifyIORef` f
 
-getUserMultiplier ::
-       ( H.Hashable u
-       , MonadIO m
-       , MonadThrow m
-       , MonadReader env m
-       , Has Logger.Handle env
-       )
-    => Handle
-    -> u
-    -> m Int
+getUserMultiplier :: (H.Hashable u, MonadIO m) => Handle -> u -> m Int
 getUserMultiplier hState user = do
     st <- hGetState hState
     let drepeats = echoMultiplier (hState :: Handle)
@@ -81,38 +53,14 @@ getUserMultiplier hState user = do
         repeats = Map.findWithDefault drepeats uhash $ userSettings st
     pure repeats
 
-getUserMultiplierM ::
-       ( H.Hashable u
-       , MonadIO m
-       , MonadThrow m
-       , MonadReader env m
-       , Has Logger.Handle env
-       )
-    => Handle
-    -> Maybe u
-    -> m Int
-getUserMultiplierM hState (Just u) = hState & getUserMultiplier $ u
-getUserMultiplierM hState Nothing = do
-    envLogInfo "No User info, returning default echo multiplier"
-    pure $ echoMultiplier (hState :: Handle)
+getUserMultiplierM :: (H.Hashable u, MonadIO m) => Handle -> Maybe u -> m Int
+getUserMultiplierM hState (Just u) = getUserMultiplier hState u
+getUserMultiplierM hState Nothing = pure $ echoMultiplier (hState :: Handle)
 
 setUserMultiplier ::
-       ( Show u
-       , H.Hashable u
-       , MonadIO m
-       , MonadThrow m
-       , MonadReader env m
-       , Has Logger.Handle env
-       )
-    => Handle
-    -> u
-    -> Int
-    -> m ()
-setUserMultiplier hState user repeats = do
-    envLogDebug $
-        "Setting echo multiplier to: " <>
-        T.tshow repeats <> "For User: " <> T.tshow user
-    hState `hSetState` \st ->
+       (Show u, H.Hashable u, MonadIO m) => Handle -> u -> Int -> m ()
+setUserMultiplier hState user repeats =
+    hModifyState hState $ \st ->
         let uhash = H.hash user
             usettings = Map.alter (const $ Just repeats) uhash $ userSettings st
          in st {userSettings = usettings}
