5effa5a6e6de111fd6c4a4d738f7b50ec508b9f5
SpaceOyster <spaceoyster@disroot.org>
2022-01-13 00:53:10 +0500

    wrapped echoMultiplier in new type UsersDB.UserData

--- src/UsersDB.hs
+++ src/UsersDB.hs
@@ -6,7 +6,11 @@ module UsersDB
     ( UsersMap(..)
     , Config(..)
     , Handle(..)
+    , UserData(..)
     , new
+    , getUserData
+    , modifyUserData
+    , setUserData
     , getUserMultiplierM
     , getUserMultiplier
     , setUserMultiplier
@@ -15,11 +19,17 @@ module UsersDB
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import qualified Data.Hashable as H
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
-import qualified Data.Map as Map (Map, alter, findWithDefault)
+import qualified Data.Map as Map (Map, alter, findWithDefault, lookup)
+import Data.Maybe (fromMaybe)
+
+newtype UserData =
+    UserData
+        { getEchoMultiplier :: Int
+        }
 
 newtype UsersMap =
     UsersMap
-        { getUsersMap :: Map.Map H.Hash Int
+        { getUsersMap :: Map.Map H.Hash UserData
         }
 
 newtype Config =
@@ -30,37 +40,54 @@ newtype Config =
 data Handle =
     Handle
         { state :: IORef UsersMap
-        , defaultEchoMultiplier :: Int
+        , defaultUserData :: UserData
         }
 
 new :: (MonadIO m) => Config -> m Handle
 new Config {defaultEchoMultiplier} = do
     state <- liftIO $ newIORef UsersMap {getUsersMap = mempty}
-    pure Handle {state, defaultEchoMultiplier}
+    let defaultUserData = UserData {getEchoMultiplier = defaultEchoMultiplier}
+    pure Handle {state, defaultUserData}
 
-hGetState :: (MonadIO m) => Handle -> m UsersMap
-hGetState hState = liftIO . readIORef $ state hState
+hGetUsersMap :: (MonadIO m) => Handle -> m UsersMap
+hGetUsersMap hDB = liftIO . readIORef $ state hDB
 
-hModifyState :: (MonadIO m) => Handle -> (UsersMap -> UsersMap) -> m ()
-hModifyState hState f = liftIO $ state hState `modifyIORef` f
+hModifyUsersMap :: (MonadIO m) => Handle -> (UsersMap -> UsersMap) -> m ()
+hModifyUsersMap hDB f = liftIO $ state hDB `modifyIORef` f
+
+getUserData :: (H.Hashable u, MonadIO m) => Handle -> u -> m UserData
+getUserData hDB user = do
+    uMap <- hGetUsersMap hDB
+    let defaultSettings = defaultUserData hDB
+        uhash = H.hash user
+        maybeSettings = Map.lookup uhash $ getUsersMap uMap
+    pure $ fromMaybe defaultSettings maybeSettings
 
 getUserMultiplier :: (H.Hashable u, MonadIO m) => Handle -> u -> m Int
-getUserMultiplier hState user = do
-    st <- hGetState hState
-    let defaultMultiplier = defaultEchoMultiplier (hState :: Handle)
-        uhash = H.hash user
-        repeats = Map.findWithDefault defaultMultiplier uhash $ getUsersMap st
-    pure repeats
+getUserMultiplier hDB user = getEchoMultiplier <$> getUserData hDB user
 
 getUserMultiplierM :: (H.Hashable u, MonadIO m) => Handle -> Maybe u -> m Int
-getUserMultiplierM hState (Just u) = getUserMultiplier hState u
-getUserMultiplierM hState Nothing =
-    pure $ defaultEchoMultiplier (hState :: Handle)
+getUserMultiplierM hDB (Just u) = getUserMultiplier hDB u
+getUserMultiplierM hDB Nothing = pure . getEchoMultiplier $ defaultUserData hDB
+
+modifyUserData ::
+       (H.Hashable u, MonadIO m)
+    => Handle
+    -> u
+    -> (UserData -> UserData)
+    -> m ()
+modifyUserData hDB user morph =
+    hModifyUsersMap hDB $ \uMap ->
+        let uhash = H.hash user
+            m = getUsersMap uMap
+            usettings = Map.alter (fmap morph) uhash m
+         in uMap {getUsersMap = usettings}
+
+setUserData ::
+       (Show u, H.Hashable u, MonadIO m) => Handle -> u -> UserData -> m ()
+setUserData hDB user us = modifyUserData hDB user $ const us
 
 setUserMultiplier ::
        (Show u, H.Hashable u, MonadIO m) => Handle -> u -> Int -> m ()
-setUserMultiplier hState user repeats =
-    hModifyState hState $ \st ->
-        let uhash = H.hash user
-            usettings = Map.alter (const $ Just repeats) uhash $ getUsersMap st
-         in st {getUsersMap = usettings}
+setUserMultiplier hDB user multiplier =
+    modifyUserData hDB user $ \us -> us {getEchoMultiplier = multiplier}
