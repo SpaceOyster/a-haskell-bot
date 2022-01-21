ea03f3f2d1501ef041642497a7a17f05f953b86e
SpaceOyster <spaceoyster@disroot.org>
2022-01-21 14:44:25 +0500

    removed Data.List.Extended module

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,3 @@
-import qualified Data.List.ExtendedSpec
 import qualified Network.URI.ExtendedSpec
 import Test.Hspec
 
@@ -7,5 +6,4 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "Data.List.Extended" Data.List.ExtendedSpec.spec
   describe "Network.URI.Extended" Network.URI.ExtendedSpec.spec
