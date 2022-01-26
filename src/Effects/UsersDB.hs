462b1c0104fefeea15d6297f789fc8bcb8370792
SpaceOyster <spaceoyster@disroot.org>
2022-01-26 23:03:37 +0500

    added some utility functions to Effects.UsersDB

--- src/Effects/UsersDB.hs
+++ src/Effects/UsersDB.hs
@@ -13,3 +13,17 @@ class Monad m =>
   defaultUserData :: m UserData
   getUserData :: H.Hashable u => u -> m UserData
   modifyUserData :: H.Hashable u => u -> (UserData -> UserData) -> m ()
+
+getUserMultiplier :: (H.Hashable u, MonadUsersDB m) => u -> m Int
+getUserMultiplier user = getEchoMultiplier <$> getUserData user
+
+getUserMultiplierM :: (H.Hashable u, MonadUsersDB m) => Maybe u -> m Int
+getUserMultiplierM (Just u) = getUserMultiplier u
+getUserMultiplierM Nothing = getEchoMultiplier <$> defaultUserData
+
+setUserData :: (H.Hashable u, MonadUsersDB m) => u -> UserData -> m ()
+setUserData user d = modifyUserData user $ const d
+
+setUserMultiplier :: (H.Hashable u, MonadUsersDB m) => u -> Int -> m ()
+setUserMultiplier user multiplier =
+  modifyUserData user $ \us -> us {getEchoMultiplier = multiplier}
