bea74415138e3fae15257cb6f265fc43849a2423
SpaceOyster <spaceoyster@disroot.org>
2022-01-26 11:01:13 +0500

    defined MonadLog instance for App

--- src/App/Monad.hs
+++ src/App/Monad.hs
@@ -1,25 +1,34 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeApplications #-}
 
 module App.Monad where
 
-import App.Env (Env(..))
+import App.Env (Env(..), grab)
 import Control.Monad.Catch (MonadThrow)
-import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (MonadReader(..), ReaderT(..))
+import qualified Effects.Log as Log
+import qualified Logger
 
 type AppEnv = Env App
 
 newtype App a =
-    App
-        { unApp :: ReaderT AppEnv IO a
-        }
-    deriving ( Functor
-             , Applicative
-             , Monad
-             , MonadIO
-             , MonadThrow
-             , MonadReader AppEnv
-             )
+  App
+    { unApp :: ReaderT AppEnv IO a
+    }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadIO
+           , MonadThrow
+           , MonadReader AppEnv
+           )
 
 runApp :: AppEnv -> App a -> IO a
 runApp env = (`runReaderT` env) . unApp
+
+instance Log.MonadLog App where
+  doLog p t = do
+    hLog <- grab @Logger.Handle
+    let logAction = Logger.getLog hLog
+    App . liftIO $ logAction p t
