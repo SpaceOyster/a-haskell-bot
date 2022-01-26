36a8fb4fdfd5ca39049c3df92785a2b59e9e3f0d
SpaceOyster <spaceoyster@disroot.org>
2022-01-26 15:17:51 +0500

    moved HTTP.Request to Effects.HTTP module

--- src/Effects/HTTP.hs
+++ src/Effects/HTTP.hs
@@ -1,1 +1,10 @@
 module Effects.HTTP where
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Network.URI.Extended as URI
+
+data Request
+  = GET URI.URI
+  | POST URI.URI L8.ByteString
+  deriving (Show)
+
--- src/HTTP.hs
+++ src/HTTP.hs
@@ -3,74 +3,70 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 
 module HTTP
-    ( Config(..)
-    , Handle(..)
-    , Request(..)
-    , new
-    ) where
+  ( Config(..)
+  , Handle(..)
+  , HTTP.Request(..)
+  , new
+  ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Has (Has(..))
+import qualified Effects.HTTP as HTTP (Request(..))
 import qualified Network.HTTP.Client as H
-    ( Manager
-    , Request(..)
-    , RequestBody(..)
-    , httpLbs
-    , newManager
-    , requestFromURI
-    , responseBody
-    )
+  ( Manager
+  , Request(..)
+  , RequestBody(..)
+  , httpLbs
+  , newManager
+  , requestFromURI
+  , responseBody
+  )
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Network.URI.Extended as URI
 
 data Config =
-    Config
-        {
-        }
+  Config
+    {
+    }
 
 newtype Handle =
-    Handle
-        { sendRequest :: Request -> IO L8.ByteString
-        }
+  Handle
+    { sendRequest :: HTTP.Request -> IO L8.ByteString
+    }
 
 instance Has Handle Handle where
-    obtain = id
+  obtain = id
 
 new :: Config -> IO Handle
 new _cfg = do
-    manager <- H.newManager tlsManagerSettings
-    pure $
-        Handle
-            { sendRequest =
-                  \req ->
-                      case req of
-                          GET uri -> get manager uri
-                          POST uri body -> post manager uri body
-            }
+  manager <- H.newManager tlsManagerSettings
+  pure $
+    Handle
+      { sendRequest =
+          \req ->
+            case req of
+              HTTP.GET uri -> get manager uri
+              HTTP.POST uri body -> post manager uri body
+      }
 
 get :: H.Manager -> URI.URI -> IO L8.ByteString
 get manager uri = do
-    req <- H.requestFromURI uri
-    let req' = bakeReq req
-    H.responseBody <$> H.httpLbs req' manager
+  req <- H.requestFromURI uri
+  let req' = bakeReq req
+  H.responseBody <$> H.httpLbs req' manager
   where
     bakeReq req = req {H.method = "GET"}
 
 post :: H.Manager -> URI.URI -> L8.ByteString -> IO L8.ByteString
 post manager uri body = do
-    req <- H.requestFromURI uri
-    let req' = bakeReq req
-    H.responseBody <$> H.httpLbs req' manager
+  req <- H.requestFromURI uri
+  let req' = bakeReq req
+  H.responseBody <$> H.httpLbs req' manager
   where
     bakeReq req =
-        req
-            { H.method = "POST"
-            , H.requestBody = H.RequestBodyLBS body
-            , H.requestHeaders =
-                  [("Content-Type", "application/json; charset=utf-8")]
-            }
-
-data Request
-    = GET URI.URI
-    | POST URI.URI L8.ByteString
-    deriving (Show)
+      req
+        { H.method = "POST"
+        , H.requestBody = H.RequestBodyLBS body
+        , H.requestHeaders =
+            [("Content-Type", "application/json; charset=utf-8")]
+        }
