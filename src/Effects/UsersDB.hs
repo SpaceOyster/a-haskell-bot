24261c333add8bc2ad411acdc1332f1dcf52296e
SpaceOyster <spaceoyster@disroot.org>
2022-01-26 23:03:00 +0500

    defined Effects.UsersDB.MonadUsersDB class

--- src/Effects/UsersDB.hs
+++ src/Effects/UsersDB.hs
@@ -1,6 +1,15 @@
 module Effects.UsersDB where
 
+import qualified Data.Hashable as H
+
 newtype UserData =
   UserData
     { getEchoMultiplier :: Int
     }
+
+class Monad m =>
+      MonadUsersDB m
+  where
+  defaultUserData :: m UserData
+  getUserData :: H.Hashable u => u -> m UserData
+  modifyUserData :: H.Hashable u => u -> (UserData -> UserData) -> m ()
