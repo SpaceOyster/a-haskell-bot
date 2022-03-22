b4e09ec911aa6526e9e12943d1076e423c3fdd6a
SpaceOyster <spaceoyster@disroot.org>
2022-03-22 22:07:53 +0500

    added App.Error.AppError data type of Exceptions

--- src/App/Error.hs
+++ src/App/Error.hs
@@ -1,43 +1,52 @@
 module App.Error where
 
-import Control.Monad.Catch (Exception(..))
+import Control.Monad.Catch (Exception (..))
+import Data.Text.Extended
 import Data.Typeable (Typeable)
 
 data Priority
-    = Debug
-    | Info
-    | Warning
-    | Error
-    deriving (Eq, Enum, Show)
+  = Debug
+  | Info
+  | Warning
+  | Error
+  deriving (Eq, Enum, Show)
 
 data BotException
-    = Ex !String
-    | URLParsing !String
-    deriving (Typeable)
+  = Ex !String
+  | URLParsing !String
+  deriving (Typeable)
 
 instance Exception BotException
 
 instance Show BotException where
-    show (Ex description) =  "Ex: " <> description
-    show (URLParsing description) = "URLParsing: " <> description
+  show (Ex description) = "Ex: " <> description
+  show (URLParsing description) = "URLParsing: " <> description
 
 data APIException
-    = APIStateSetting !String
-    | APIRespondedWithError !String
-    | VKPollError !String
+  = APIStateSetting !String
+  | APIRespondedWithError !String
+  | VKPollError !String
 
 instance Exception APIException
 
 instance Show APIException where
-    show (APIStateSetting description) = "APIStateSetting: " <> description
-    show (APIRespondedWithError description) =
-        "APIRespondedWithError: " <> description
-    show (VKPollError c) = "VKPollError: Code " <> c
+  show (APIStateSetting description) = "APIStateSetting: " <> description
+  show (APIRespondedWithError description) =
+    "APIRespondedWithError: " <> description
+  show (VKPollError c) = "VKPollError: Code " <> c
 
-data ParsingException =
-    ParsingException !String
+data ParsingException
+  = ParsingException !String
 
 instance Exception ParsingException
 
 instance Show ParsingException where
-    show (ParsingException description) = "ParsingException: " <> description
+  show (ParsingException description) = "ParsingException: " <> description
+
+data AppError = NotFound | APIUnexpectedResponse Text
+  deriving (Show)
+
+instance Exception AppError
+
+apiUnexpectedResponse :: Text -> AppError
+apiUnexpectedResponse = APIUnexpectedResponse
