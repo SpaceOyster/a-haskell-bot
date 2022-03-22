3799c8004bcf05e39fd0103bd81702bedde432ec
SpaceOyster <spaceoyster@disroot.org>
2022-03-22 22:06:37 +0500

    added Data.Text.Extended.lazyDecodeUtf8 function

--- src/Data/Text/Extended.hs
+++ src/Data/Text/Extended.hs
@@ -1,9 +1,16 @@
 module Data.Text.Extended
-    ( module Data.Text
-    , tshow
-    ) where
+  ( module Data.Text,
+    tshow,
+    lazyDecodeUtf8,
+  )
+where
 
+import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Text
+import qualified Data.Text.Encoding as TE
 
 tshow :: Show a => a -> Text
 tshow = pack . show
+
+lazyDecodeUtf8 :: L8.ByteString -> Text
+lazyDecodeUtf8 = TE.decodeUtf8 . L8.toStrict
