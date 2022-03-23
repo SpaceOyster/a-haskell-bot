5e210354e9309080b22a645ea2dc02b59185d155
SpaceOyster <spaceoyster@disroot.org>
2022-03-23 23:47:24 +0500

    removed old error types from App.Error

--- src/App/Error.hs
+++ src/App/Error.hs
@@ -2,7 +2,6 @@ module App.Error where
 
 import Control.Monad.Catch (Exception (..))
 import Data.Text.Extended
-import Data.Typeable (Typeable)
 
 data Priority
   = Debug
@@ -10,38 +9,6 @@ data Priority
   | Warning
   | Error
   deriving (Eq, Enum, Show)
-
-data BotException
-  = Ex !String
-  | URLParsing !String
-  deriving (Typeable)
-
-instance Exception BotException
-
-instance Show BotException where
-  show (Ex description) = "Ex: " <> description
-  show (URLParsing description) = "URLParsing: " <> description
-
-data APIException
-  = APIStateSetting !String
-  | APIRespondedWithError !String
-  | VKPollError !String
-
-instance Exception APIException
-
-instance Show APIException where
-  show (APIStateSetting description) = "APIStateSetting: " <> description
-  show (APIRespondedWithError description) =
-    "APIRespondedWithError: " <> description
-  show (VKPollError c) = "VKPollError: Code " <> c
-
-data ParsingException
-  = ParsingException !String
-
-instance Exception ParsingException
-
-instance Show ParsingException where
-  show (ParsingException description) = "ParsingException: " <> description
 
 data AppError
   = NotFound
