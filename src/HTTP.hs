6b8cb98d1bdfc52fdf361d6a463d5bceacd50f1a
SpaceOyster <spaceoyster@disroot.org>
2022-03-24 16:50:07 +0500

    HTTP.Handle now converts exceptions to AppError.HTTPError

--- src/HTTP.hs
+++ src/HTTP.hs
@@ -1,38 +1,41 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module HTTP
-  ( Config(..)
-  , Handle(..)
-  , HTTP.Request(..)
-  , new
-  ) where
+  ( Config (..),
+    Handle (..),
+    HTTP.Request (..),
+    new,
+  )
+where
 
+import App.Error as Err
+import Control.Monad.Catch (MonadThrow, catch, throwM)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Has (Has(..))
-import qualified Effects.HTTP as HTTP (Request(..))
+import Data.Has (Has (..))
+import qualified Data.Text.Extended as T
+import qualified Effects.HTTP as HTTP (Request (..))
 import qualified Network.HTTP.Client as H
-  ( Manager
-  , Request(..)
-  , RequestBody(..)
-  , httpLbs
-  , newManager
-  , requestFromURI
-  , responseBody
+  ( HttpException,
+    Manager,
+    Request (..),
+    RequestBody (..),
+    httpLbs,
+    newManager,
+    requestFromURI,
+    responseBody,
   )
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import qualified Network.URI.Extended as URI
 
-data Config =
-  Config
-    {
-    }
+data Config = Config
+  {
+  }
 
-newtype Handle =
-  Handle
-    { sendRequest :: HTTP.Request -> IO L8.ByteString
-    }
+newtype Handle = Handle
+  { sendRequest :: HTTP.Request -> IO L8.ByteString
+  }
 
 instance Has Handle Handle where
   obtain = id
@@ -40,14 +43,19 @@ instance Has Handle Handle where
 new :: Config -> IO Handle
 new _cfg = do
   manager <- H.newManager tlsManagerSettings
-  pure $
-    Handle
-      { sendRequest =
-          \req ->
-            case req of
-              HTTP.GET uri -> get manager uri
-              HTTP.POST uri body -> post manager uri body
-      }
+  pure $ Handle {sendRequest = \req -> sendWith manager req `catch` rethrow}
+  where
+    rethrow :: MonadThrow m => H.HttpException -> m a
+    rethrow = throwM . httpToAppError
+
+sendWith :: H.Manager -> HTTP.Request -> IO L8.ByteString
+sendWith manager req =
+  case req of
+    HTTP.GET uri -> get manager uri
+    HTTP.POST uri body -> post manager uri body
+
+httpToAppError :: H.HttpException -> Err.AppError
+httpToAppError = HTTPError . T.tshow
 
 get :: H.Manager -> URI.URI -> IO L8.ByteString
 get manager uri = do
@@ -65,8 +73,8 @@ post manager uri body = do
   where
     bakeReq req =
       req
-        { H.method = "POST"
-        , H.requestBody = H.RequestBodyLBS body
-        , H.requestHeaders =
+        { H.method = "POST",
+          H.requestBody = H.RequestBodyLBS body,
+          H.requestHeaders =
             [("Content-Type", "application/json; charset=utf-8")]
         }
