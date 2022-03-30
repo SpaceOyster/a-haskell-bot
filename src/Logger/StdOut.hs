86a60b1a15beab76020166a99c246e885a81a849
SpaceOyster <spaceoyster@disroot.org>
2022-03-30 23:11:10 +0500

    added IOError catching and rethrowing to Logger.StdOut

--- src/Logger/StdOut.hs
+++ src/Logger/StdOut.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Logger.StdOut (module Logger.Internal, withHandle, new, close) where
 
+import qualified App.Error
 import Control.Exception (bracket)
 import Control.Monad (when)
+import Control.Monad.Catch (MonadThrow, catch, throwM)
+import Data.Text.Extended as T (Text, tshow)
 import qualified Data.Text.IO as T (hPutStrLn)
-import qualified Effects.Log as Log (Verbosity, composeMessage)
+import qualified Effects.Log as Log (Priority, Verbosity, composeMessage)
 import Logger.Internal
 import qualified System.IO as IO (hFlush, stdout)
 
@@ -13,11 +17,22 @@ withHandle :: Log.Verbosity -> (Handle -> IO ()) -> IO ()
 withHandle v = bracket (new v) close
 
 new :: Log.Verbosity -> IO Handle
-new v = do
+new verbosity = new_ verbosity `catch` rethrow
+  where
+    rethrow :: MonadThrow m => IOError -> m a
+    rethrow = throwM . App.Error.loggerError . ("Logger Initiation Error: " <>) . T.tshow
+
+new_ :: Log.Verbosity -> IO Handle
+new_ v = do
   let hStdout = IO.stdout
-  let getLog =
-        \p t -> when (p >= v) $ Log.composeMessage p t >>= T.hPutStrLn hStdout
+  let doLog :: Log.Priority -> Text -> IO ()
+      doLog p t = Log.composeMessage p t >>= T.hPutStrLn hStdout
+  let getLog :: Log.Priority -> Text -> IO ()
+      getLog p t = when (p >= v) (doLog p t) `catch` rethrow
   pure $ Handle {getLog}
+  where
+    rethrow :: MonadThrow m => IOError -> m a
+    rethrow = throwM . App.Error.loggerError . ("Logger failed to write message to StdOut: " <>) . T.tshow
 
 close :: Handle -> IO ()
 close _hLog = IO.hFlush IO.stdout
