1f51073882e555ef2a073fe237988886c6c51b82
SpaceOyster <spaceoyster@disroot.org>
2022-03-30 23:22:35 +0500

    cleaned up Logger.StdOut.new_ a bit

--- src/Logger/StdOut.hs
+++ src/Logger/StdOut.hs
@@ -11,7 +11,7 @@ import Data.Text.Extended as T (Text, tshow)
 import qualified Data.Text.IO as T (hPutStrLn)
 import qualified Effects.Log as Log (Priority, Verbosity, composeMessage)
 import Logger.Internal
-import qualified System.IO as IO (hFlush, stdout)
+import qualified System.IO as IO (Handle, hFlush, stdout)
 
 withHandle :: Log.Verbosity -> (Handle -> IO ()) -> IO ()
 withHandle v = bracket (new v) close
@@ -25,14 +25,15 @@ new verbosity = new_ verbosity `catch` rethrow
 new_ :: Log.Verbosity -> IO Handle
 new_ v = do
   let hStdout = IO.stdout
-  let doLog :: Log.Priority -> Text -> IO ()
-      doLog p t = Log.composeMessage p t >>= T.hPutStrLn hStdout
   let getLog :: Log.Priority -> Text -> IO ()
-      getLog p t = when (p >= v) (doLog p t) `catch` rethrow
+      getLog p t = when (p >= v) (doLog_ hStdout p t) `catch` rethrow
   pure $ Handle {getLog}
   where
     rethrow :: MonadThrow m => IOError -> m a
     rethrow = throwM . App.Error.loggerError . ("Logger failed to write message to StdOut: " <>) . T.tshow
+
+doLog_ :: IO.Handle -> Log.Priority -> Text -> IO ()
+doLog_ hStdout p t = Log.composeMessage p t >>= T.hPutStrLn hStdout
 
 close :: Handle -> IO ()
 close _hLog = IO.hFlush IO.stdout
