4b953093c505dcbdb5d9f87a7848ac373c851925
SpaceOyster <spaceoyster@disroot.org>
2022-04-25 15:28:50 +0500

    added default overlappable MonadBotReplies instance for MonadTrans

--- src/Effects/BotReplies.hs
+++ src/Effects/BotReplies.hs
@@ -1,12 +1,21 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Effects.BotReplies where
 
 import qualified Bot.Replies as Bot
+import Control.Monad.Trans (MonadTrans, lift)
 import qualified Data.Text as T
 
-class (Monad m) =>
-      MonadBotReplies m
-  where
+class (Monad m) => MonadBotReplies m where
   getReplies :: m Bot.Replies
 
 getReply :: (MonadBotReplies m) => (Bot.Replies -> T.Text) -> m T.Text
 getReply f = f <$> getReplies
+
+instance
+  {-# OVERLAPPABLE #-}
+  (MonadBotReplies m, MonadTrans t, Monad (t m)) =>
+  MonadBotReplies (t m)
+  where
+  getReplies = lift getReplies
