98e3e64c509e62ae0fae0348a1a8a3e1769238ec
SpaceOyster <spaceoyster@disroot.org>
2022-04-25 15:28:12 +0500

    added default overlappable MonadHTTP instance for MonadTrans

--- src/Effects/HTTP.hs
+++ src/Effects/HTTP.hs
@@ -1,5 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module Effects.HTTP where
 
+import Control.Monad.Trans (MonadTrans, lift)
 import qualified Data.ByteString.Lazy.Char8 as L8
 import qualified Network.URI.Extended as URI
 
@@ -8,7 +12,12 @@ data Request
   | POST URI.URI L8.ByteString
   deriving (Show)
 
-class Monad m =>
-      MonadHTTP m
-  where
+class Monad m => MonadHTTP m where
   sendRequest :: Request -> m L8.ByteString
+
+instance
+  {-# OVERLAPPABLE #-}
+  (MonadHTTP m, MonadTrans t, Monad (t m)) =>
+  MonadHTTP (t m)
+  where
+  sendRequest = lift . sendRequest
