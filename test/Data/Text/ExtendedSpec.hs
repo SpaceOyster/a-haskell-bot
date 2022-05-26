b0781ab8ab2cbc2a419a950e4f9df2ca7e87e237
SpaceOyster <spaceoyster@disroot.org>
2022-05-26 22:44:34 +0500

    fixed ghc warnings for test.Data.Text.ExtendedSpec

--- test/Data/Text/ExtendedSpec.hs
+++ test/Data/Text/ExtendedSpec.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Text.ExtendedSpec (spec) where
 
-import Test.Hspec (Spec,describe, it, shouldBe, context, pending, shouldReturn)
+import Test.Hspec (Spec,describe, it, shouldBe)
 import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Text.Extended
+import Data.Text.Extended ( Text, lazyDecodeUtf8, tshow )
 
 spec :: Spec
 spec = describe "Data.Text.Extended" $ do
@@ -13,10 +13,10 @@ spec = describe "Data.Text.Extended" $ do
 tshowSpec :: Spec
 tshowSpec = describe "tshow" $ do
   it "converts type of Show class to Text" $ do
-    tshow (Just "some") `shouldBe` ("Just \"some\"" :: Text)
-    tshow (Just [1,2,3,4]) `shouldBe` ("Just [1,2,3,4]" :: Text)
-    tshow [1,2,3,4] `shouldBe` ("[1,2,3,4]" :: Text)
-    tshow 1234 `shouldBe` ("1234" :: Text)
+    tshow (Just ("some" :: String)) `shouldBe` ("Just \"some\"" :: Text)
+    tshow (Just [1:: Integer,2,3,4]) `shouldBe` ("Just [1,2,3,4]" :: Text)
+    tshow [1:: Integer,2,3,4] `shouldBe` ("[1,2,3,4]" :: Text)
+    tshow (1234 :: Integer) `shouldBe` ("1234" :: Text)
 
 lazyDecodeUtf8Spec :: Spec
 lazyDecodeUtf8Spec = describe "lazyDecodeUtf8" $ do
