d0670c9eb9fc04d1890fc1f0cea0479a7c5a1e9c
SpaceOyster <spaceoyster@disroot.org>
2022-05-31 10:56:17 +0500

    test.API.Telegram.TypesSpec changed with `shouldReturn`

--- test/API/Telegram/TypesSpec.hs
+++ test/API/Telegram/TypesSpec.hs
@@ -1,11 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module API.Telegram.TypesSpec
-  ( spec
-  ) where
+  ( spec,
+  )
+where
 
 import API.Telegram.Types as TG
+  ( Error (Error),
+    Response (ErrorResponse, ResponseWith),
+    fromResponse,
+  )
+import Data.Aeson as Aeson (ToJSON (toJSON))
 import Test.Hspec
-import Data.Aeson as Aeson
+  ( Spec,
+    anyException,
+    context,
+    describe,
+    it,
+    shouldReturn,
+    shouldThrow,
+  )
 
 spec :: Spec
 spec = describe "API.Telegram.Types" $ do
@@ -15,17 +29,17 @@ fromResponseSpec :: Spec
 fromResponseSpec = describe "fromResponse" $ do
   context "when Telegram API responded with json" $
     it "extracts data of Aeson.ToJSON class from API.Telegram.Response" $ do
-      fromResponse (ResponseWith $ Aeson.toJSON ([1,2,3,4] :: [Integer])) 
-        `shouldBe` (Just [1,2,3,4] :: Maybe [Integer])
-      fromResponse (ResponseWith $ Aeson.toJSON True) 
-        `shouldBe` (Just True :: Maybe Bool)
+      fromResponse (ResponseWith $ Aeson.toJSON ([1, 2, 3, 4] :: [Integer]))
+        `shouldReturn` ([1, 2, 3, 4] :: [Integer])
+      fromResponse (ResponseWith $ Aeson.toJSON True)
+        `shouldReturn` True
   context "when json can't be parsed to expected data type" $
     it "throws error" $ do
-      (fromResponse (ResponseWith $ Aeson.toJSON True) :: IO Integer) 
+      (fromResponse (ResponseWith $ Aeson.toJSON True) :: IO Integer)
         `shouldThrow` anyException
-      (fromResponse (ResponseWith $ Aeson.toJSON ['a','b']) :: IO Bool) 
+      (fromResponse (ResponseWith $ Aeson.toJSON ['a', 'b']) :: IO Bool)
         `shouldThrow` anyException
   context "when Telegram API responded with error" $
     it "throws error" $ do
-      (fromResponse (ErrorResponse $ TG.Error 101 "sommin bad happn :(") :: IO Bool) 
+      (fromResponse (ErrorResponse $ TG.Error 101 "sommin bad happn :(") :: IO Bool)
         `shouldThrow` anyException
