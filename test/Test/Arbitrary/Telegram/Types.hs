0f01dd05332d83ddc3f7c0eea69e8c9b2468464f
SpaceOyster <spaceoyster@disroot.org>
2022-06-09 23:57:50 +0500

    added Test.Arbitrary.Telegram.Types

---
+++ test/Test/Arbitrary/Telegram/Types.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Test.Arbitrary.Telegram.Types where
+
+import API.Telegram.Monad (Config (..), TGState (..))
+import API.Telegram.Types as TG
+  ( CallbackQuery (..),
+    Chat (Chat),
+    Error (..),
+    Message (..),
+    Update (..),
+    User (User),
+  )
+import Network.URI (URI (uriPath))
+import Test.Arbitrary.String (CleanString (getCleanString))
+import Test.Arbitrary.Text (AnyText (getAnyText))
+import Test.QuickCheck (Arbitrary (arbitrary))
+
+instance Arbitrary TG.Message where
+  arbitrary = do
+    message_id <- arbitrary
+    from <- arbitrary
+    chat <- arbitrary
+    date <- arbitrary
+    text <- fmap getAnyText <$> arbitrary
+    pure TG.Message {..}
+
+instance Arbitrary TG.CallbackQuery where
+  arbitrary = do
+    cq_id <- getAnyText <$> arbitrary
+    from <- arbitrary
+    message <- arbitrary
+    inline_message_id <- fmap getAnyText <$> arbitrary
+    query_data <- fmap getAnyText <$> arbitrary
+    pure TG.CallbackQuery {..}
+
+instance Arbitrary TG.User where
+  arbitrary = User <$> arbitrary
+
+instance Arbitrary TG.Chat where
+  arbitrary = Chat <$> arbitrary
+
+instance Arbitrary TG.Update where
+  arbitrary = do
+    update_id <- arbitrary
+    message <- arbitrary
+    callback_query <- arbitrary
+    pure TG.Update {..}
+
+instance Arbitrary TG.Error where
+  arbitrary = do
+    error_code <- arbitrary
+    description <- getAnyText <$> arbitrary
+    pure TG.Error {..}
+
+instance Arbitrary Config where
+  arbitrary = do
+    key <- getCleanString <$> arbitrary
+    timeout_seconds <- arbitrary
+    pure Config {..}
+
+instance Arbitrary TGState where
+  arbitrary = do
+    lastUpdate <- arbitrary
+    key <- arbitrary
+    let apiURI = tgAPIURI {uriPath = uriPath tgAPIURI <> key <> "/"}
+    timeout <- arbitrary
+    pure TGState {..}
