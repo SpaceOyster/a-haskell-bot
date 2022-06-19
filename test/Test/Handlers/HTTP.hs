15e52a7cac99dc935ae4c71ceea2a78a8373ae44
SpaceOyster <spaceoyster@disroot.org>
2022-06-19 15:29:06 +0500

    added Test.Handlers.HTTP.modelHTTPReplies

--- test/Test/Handlers/HTTP.hs
+++ test/Test/Handlers/HTTP.hs
@@ -9,8 +9,23 @@ import qualified Handlers.HTTP as HTTP
 newWithSingle :: L8.ByteString -> HTTP.Handle
 newWithSingle bs = HTTP.Handle {HTTP.sendRequest = pure . const bs}
 
+newWithQueue :: (MonadIO m) => [L8.ByteString] -> m HTTP.Handle
+newWithQueue bss = do
+  let repliesSeq = Seq.fromList bss
+      len = Seq.length repliesSeq
+  nextReplyIndexRef <- liftIO $ newIORef (0 :: Int)
+  let sendRequest = \_ -> do
+        replyIndex <- readIORef nextReplyIndexRef
+        modifyIORef' nextReplyIndexRef ((`mod` len) . (+ 1))
+        pure (Seq.index repliesSeq replyIndex)
+  pure $ HTTP.Handle {HTTP.sendRequest = sendRequest}
 
 modelHTTPReply :: (MonadIO m) => L8.ByteString -> (HTTP.Handle -> m a) -> m a
 modelHTTPReply replyBS run = do
   let http = newWithSingle replyBS
+  run http
+
+modelHTTPReplies :: (MonadIO m) => [L8.ByteString] -> (HTTP.Handle -> m a) -> m a
+modelHTTPReplies repliesBS run = do
+  http <- newWithQueue repliesBS
   run http
