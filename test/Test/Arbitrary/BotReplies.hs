473ea7cf17d9b7e31052df48ccd38c1e61f0addd
SpaceOyster <spaceoyster@disroot.org>
2022-07-10 12:59:39 +0500

    resized test.Test.Arbitrary.BotReplies Replies generator

--- test/Test/Arbitrary/BotReplies.hs
+++ test/Test/Arbitrary/BotReplies.hs
@@ -6,10 +6,10 @@ module Test.Arbitrary.BotReplies where
 
 import qualified Effects.BotReplies as BR
 import Test.Arbitrary.Text (AnyText (getAnyText))
-import Test.QuickCheck (Arbitrary (arbitrary))
+import Test.QuickCheck (Arbitrary (arbitrary), resize)
 
 instance Arbitrary BR.Replies where
-  arbitrary = do
+  arbitrary = resize 5 $ do
     help <- getAnyText <$> arbitrary
     greeting <- getAnyText <$> arbitrary
     repeat <- getAnyText <$> arbitrary
